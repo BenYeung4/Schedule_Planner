913a62faf0b9e3a94f9791f691770b7865d2c521
Ben <byeungis@gmail.com>
2022-05-14 01:33:19 -0400

    added

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+//showing the current date at the top
+var currentDay = document.getElementById("currentDay");
+
+var currentDay = new Date();
+var day = (today.getMonth()+1)+'-'+today.getFullYear()+'-'+today.getDate();
+dateToday.textContent = day;

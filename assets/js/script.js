d09a4a632b5f67467898d4a88bcecb59904dc079
Ben <ben@Benjamins-MacBook-Pro.local>
2022-05-15 18:21:05 -0400

    Javascript working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 
 //showing the current date at the top
-var currentDay = document.getElementById("#currentDay");
 
-var currentDay = new Date();
-var day = (today.getMonth()+1)+'-'+today.getFullYear()+'-'+today.getDate();
-dateToday.textContent = day;
+
+var today = new Date();
+var day = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+var time = today.getHours() +":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = day + " " + time;
+
+document.getElementById("currentDay").innerHTML = dateTime;
+
+console.log(today);
